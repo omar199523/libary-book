38c8bd69b960408738ce4b87cf561aa84d2d0e5f
omar199523 <model199523@github.com>
2021-10-10 11:26:26 +0300

    edit some style book and and edit some code js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const shilf_1=document.getElementById("shilf1"),
         shilf_2=document.getElementById("shilf2"),
-        shilf_3=document.getElementById("shilf3");
+        shilf_3=document.getElementById("shilf3"),
+        libaryContiner=document.getElementById("libaryContiner"),
+        addForm=document.getElementById("addForm")
 
 let books = [
     {
@@ -19,6 +21,24 @@ let books = [
         image: "https://img.com/bookimg.jpeg"
     }
 ]
+document.getElementById('newBook').addEventListener('click',()=>{
+    libaryContiner.style.display= "none";
+    addForm.style.display= "flex";
+
+})
+addForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const newBook ={
+        title:document.getElementById("bookTitle").value,
+        auther:document.getElementById("bookAuthor").value,
+        image:document.getElementById("bookImg").value
+    }
+    console.log(newBook)
+    books.push(newBook)
+    createAllBooks(books);
+    libaryContiner.style.display= "flex";
+    addForm.style.display= "none";
+})
 
 const createOneBook = (parent,{title,author,image})=>{
     const bookCont =document.createElement('div');
@@ -35,6 +55,9 @@ const createOneBook = (parent,{title,author,image})=>{
     parent.append(bookCont);
 }
 const createAllBooks = (arr) => {
+    shilf_1.innerHTML="";
+    shilf_2.innerHTML="";
+    shilf_3.innerHTML="";
     const sortArrToAlAlphab = arr.sort((a,b)=>{a-b});
     console.log(sortArrToAlAlphab)
     sortArrToAlAlphab.map((book,index,arr)=>{
